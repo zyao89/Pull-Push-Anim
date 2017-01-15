9e6a3afb639b6654ec08eeeaa87dd1b4d0725544
Zyao89 <305161066@qq.com>
2017-01-16 01:35:38 +0800

    新增忽略文件

--- zcore/src/main/java/com/zyao89/framework/zcore/base/BasePresenter.java
+++ zcore/src/main/java/com/zyao89/framework/zcore/base/BasePresenter.java
@@ -1,9 +1,9 @@
 package com.zyao89.framework.zcore.base;
 
 /**
+ * 中间件基类
  * Created by zyao89 on 2017/1/13.
  */
-
 public abstract class BasePresenter<V extends IBaseViewHandler> implements IBasePresenter {
 
     private V mViewHandler;
